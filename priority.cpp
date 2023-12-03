6d099643742e269d179be655f0f6b6b20b8349b9
Vineet Kumar Kankerwal <codevkankerwal@gmail.com>
2023-12-03 22:40:36 +0530

    non preemptive priority scheduling added

--- priority.cpp
+++ priority.cpp
@@ -3,9 +3,8 @@
 #include <iomanip>
 using namespace std;
 
-void SJF(vector<string> pid, vector<int> arrivalTime, vector<int> burstTime)
+void Priority(vector<string> &pid, vector<int> &_priority, vector<int> &arrivalTime, vector<int> &burstTime)
 {
-    //first sort with respect of arrival time
     for (int i = 0; i < pid.size(); i++)
     {
         for (int j = i + 1; j < pid.size(); j++)
@@ -13,13 +12,14 @@ void SJF(vector<string> pid, vector<int> arrivalTime, vector<int> burstTime)
             if (arrivalTime[i] > arrivalTime[j])
             {
                 swap(pid[i], pid[j]);
+                swap(_priority[i], _priority[j]);
                 swap(arrivalTime[i], arrivalTime[j]);
                 swap(burstTime[i], burstTime[j]);
                 // pid id, arrival time as well as burst time of the pides are sorted inaccordance with arrival time.
             }
         }
     }
-    //after sorting with respect of arrival time check if there is any process with same arrival time, if yes- sort it with respect of burst time
+
     for (int i = 0; i < pid.size(); i++)
     {
         for (int j = i + 1; j < pid.size(); j++)
@@ -27,7 +27,7 @@ void SJF(vector<string> pid, vector<int> arrivalTime, vector<int> burstTime)
             if (arrivalTime[i] == arrivalTime[j])
             // Condition is checked if two pides have same arrival time.
             {
-                if (burstTime[i] > burstTime[j])
+                if (_priority[i] > _priority[j])
                 {
                     swap(pid[i], pid[j]);
                     swap(arrivalTime[i], arrivalTime[j]);
@@ -68,10 +68,10 @@ void SJF(vector<string> pid, vector<int> arrivalTime, vector<int> burstTime)
                 continue;
             else
             {
-                if (arrivalTime[j] < temp && burstTime[j] < low)
+                if (arrivalTime[j] < temp && _priority[j] < low)
                 // Completion time of previous process is compare with the arrival time of current process as well the burst time is compared.
                 {
-                    low = burstTime[j];
+                    low = _priority[j];
                     value = j;
                     //  avt = arrivalTime[j];
                 }
@@ -84,17 +84,18 @@ void SJF(vector<string> pid, vector<int> arrivalTime, vector<int> burstTime)
         check.push_back(pid[value]);
     }
 
-    cout << "Process Id" << setw(20) << "Arrival Time" << setw(20) << "Burst Time" << setw(20) << "Completion Time" << setw(20) << " Turn Around" << setw(20) << "Waiting Time" << endl;
+    cout << "Process Id" << setw(20) << "Priority" << setw(20) << "Arrival Time" << setw(20) << "Burst Time" << setw(20) << "Completion Time" << setw(20) << " Turn Around" << setw(20) << "Waiting Time" << endl;
     for (int i = 0; i < pid.size(); i++)
     {
-        cout << pid[i] << setw(20) << arrivalTime[i] << setw(20) << burstTime[i] << setw(20) << completion_time[i] << setw(20) << turn_around_time[i] << setw(20) << waiting_time[i] << endl;
+        cout << pid[i] << setw(20) << _priority[i] << setw(20) << arrivalTime[i] << setw(20) << burstTime[i] << setw(20) << completion_time[i] << setw(20) << turn_around_time[i] << setw(20) << waiting_time[i] << endl;
     }
 }
 
 int main()
 {
-    vector<string> pid = {"p1", "p2", "p3", "p4"};
-    vector<int> arrivalTime = {1, 2, 1, 4};
-    vector<int> burstTime = {3, 4, 2, 4};
-    SJF(pid, arrivalTime, burstTime);
+    vector<string> pid = {"p1", "p2", "p3", "p4", "p5", "p6", "p7"};
+    vector<int> priority = {3, 4, 4, 5, 2, 6, 1};
+    vector<int> at = {0, 1, 3, 4, 5, 6, 10};
+    vector<int> bt = {8, 2, 4, 1, 6, 5, 1};
+    Priority(pid, priority, at, bt);
 }
